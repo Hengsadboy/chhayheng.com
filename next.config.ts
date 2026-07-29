d9471d6565a9ea70c664447cff3b10340f1fdb75
Harik-Valentine <henglop1999@gmail.com>
2026-07-29 18:41:03 +0700

    Optimize Next.js build speed (disable browser source maps)

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   compress: true,
   reactStrictMode: false,
+  productionBrowserSourceMaps: false,
   typescript: {
     ignoreBuildErrors: true,
   },
